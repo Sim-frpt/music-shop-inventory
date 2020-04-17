a2beff69a06080139c18e3049cdfa0c436bafa68
Simon Fraipont <simon.fraipont@gmail.com>
2020-04-17 16:38:33 +0200

    add category creation form and controller (POST & GET) and cleanup category details contoller

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -1,12 +1,54 @@
 const db = require('../db/index');
+const { check, validationResult } = require('express-validator');
+const baseCategoryUrl = "/inventory/category";
 
 exports.getCategoryCreateForm = (req, res) => {
-  res.send("Get create form, not implemented yet");
+  res.render("category-form", { title: "Create Category" });
 };
 
-exports.postCategoryCreateForm = (req, res) => {
-  res.send("Post create form , not implemented yet");
-};
+exports.postCategoryCreateForm = [
+  check('name')
+    .isLength({ min:2, max: 50}).withMessage("Name must be between 2 and 50 characters")
+    .not().isEmpty().withMessage("Name must not be empty")
+    .trim()
+    .isAlpha().withMessage("Name must not contain numbers")
+    .escape()
+, (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+
+    return res.render("category-form", {
+      title: "Create Category",
+      catInput: req.body.name,
+      errors: errors.array()
+    });
+  }
+
+  const existingCatQuery = {
+    text: "SELECT category_id FROM category WHERE name = $1",
+    values: [ req.body.name ]
+  };
+
+  const createCatQuery = {
+    text: "INSERT INTO category(name) VALUES($1) RETURNING category_id",
+    values: [ req.body.name.toLowerCase() ]
+  };
+
+  db.query(existingCatQuery)
+    .then(result => {
+      if (result.rows.length > 0) {
+        return res.redirect(baseCategoryUrl + "/" + result.rows[0].category_id);
+      }
+
+      db.query(createCatQuery)
+        .then(result => {
+          return res.redirect(baseCategoryUrl + "/" + result.rows[0].category_id);
+        })
+        .catch(err => next(err));
+    })
+    .catch(err => next(err));
+}];
 
 exports.getCategoryUpdateForm = (req, res) => {
   res.send("Get update form, not implemented yet");
@@ -25,24 +67,32 @@ exports.postCategoryDeleteForm = (req, res) => {
 };
 
 exports.getCategoryDetails = (req, res, next) => {
-  const query = {
-    text: "SELECT i.name as instrument, instrument_id, c.name FROM category c LEFT JOIN instrument i ON c.category_id = i.category_id WHERE c.category_id = $1",
-    values: [req.params.id]
+  const categoryQuery = {
+    text: "SELECT * from category WHERE category_id = $1",
+    values: [ req.params.id ]
   };
 
-  db.query(query)
-    .then(result => {
-      if (!result.rows) {
+  const instrumentsQuery = {
+    text: "SELECT instrument_id, name FROM instrument WHERE category_id = $1",
+    values: [ req.params. id ]
+  };
+
+  Promise.all([ db.query(categoryQuery), db.query(instrumentsQuery) ])
+    .then(results => {
+      const category = results[0].rows[0];
+      const instruments = results[1].rows;
+
+      if (!category) {
         const error = new Error("Category not found");
         error.status = 404;
 
-        next(error);
+        return next(error);
       }
-      console.log(result.rows);
+
       res.render("category-details", {
         title: "Category Details",
-        categoryName: result.rows[0].name,
-        instruments: result.rows
+        category,
+        instruments
       });
     })
     .catch(err => next(err));
@@ -53,7 +103,9 @@ exports.getCategoriesList = (req, res, next) => {
     .then(result => {
       res.render('categories', {
         title: "Categories List",
-        categories: result.rows
+        categories: result.rows,
+        url: "/inventory/category",
+
       });
     })
     .catch(err => {
